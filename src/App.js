4e41d69785ddf2e10750c770e746adf39c0bc25e
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-05-17 19:20:52 +0530

    created first statefull class Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import {Rishabh} from './components/Rishabh';
-
+import Rishi from './components/Rishi';
 function App() {
   return (
     <div className="App">
       <Rishabh/>
+      <Rishi/>
       </div>
   );
 }
---
+++ src/components/Rishi.js
@@ -0,0 +1,7 @@
+import React,{Component} from "react";
+class Rishi extends Component{
+    render(){
+        return <h2>Hello Rishi and welcome to concept of Class Component</h2>
+    }
+}
+export default Rishi
