08c718072312c8fb80eba9758bc31458091224a9
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-05-20 19:15:37 +0530

    Conditional Rendering using 2nd Approach Element variable

--- src/components/ConditionalRendering.js
+++ src/components/ConditionalRendering.js
@@ -4,15 +4,21 @@ class ConditionalRendering extends Component {
       super(props)
     
       this.state = {
-         isLoggedIn: false
+         isLoggedIn: true
       }
     }
   render() {
-    if(this.state.isLoggedIn)
-        return <div>Hello and welcome Rishabh</div>
-        else
-        return <div>Hello and Welcome Guest</div>
+    // if(this.state.isLoggedIn)
+    //     return <div>Hello and welcome Rishabh</div>
+    //     else
+    //     return <div>Hello and Welcome Guest</div>
     
+    let Message
+    if(this.state.isLoggedIn)
+        Message=<div>Hello and welcome Rishabh</div>
+        else
+        Message=<div>Hello and welcome Guest</div>
+        return <div>{Message}</div>
   }
 }
 
