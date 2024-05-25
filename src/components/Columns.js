bb4c1989beaa166c4a82af59796b0b42b23f1beb
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-05-25 18:09:22 +0530

    React Fragments with key attribute and short hand approach using <></>

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
 function Columns() {
+    const items= []
   return (
-    <React.Fragment><td>Name</td>
+    <>
+            {/* {
+                items.map(item=>(
+                    <React.Fragment key={item.id}> 
+                        <h1>Title</h1>
+                    <p>{item.Title}</p>
+                    </React.Fragment>
+                ))
+            } */}
+       
+    <td>Name</td>
     <td>Rishabh</td>
-    </React.Fragment>
+    </>
     )
 }
 export default Columns
