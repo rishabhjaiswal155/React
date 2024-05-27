a8156c986efea1f615134f0d9879373aa42bc52e
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-05-27 14:37:54 +0530

    React memo in Functional Component(just like pure Component)

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,8 +1,9 @@
-import React, { Component ,PureComponent} from 'react'
+import React, { Component} from 'react'
 import RegComp from './RegComp'
-import PureComp from '../PureComp'
+import PureComp from './PureComp'
+import MemoComp from './MemoComp'
 
-class ParentComp extends PureComponent {
+class ParentComp extends Component {
     constructor(props) {
       super(props)
     
@@ -20,8 +21,9 @@ class ParentComp extends PureComponent {
     console.log('*********parenet Component render*************')
     return (
       <div>Parent Component
-        <RegComp name={this.state.name}/>
-        <PureComp name={this.state.name}/>
+        <MemoComp name={this.state.name}/>
+        {/* <RegComp name={this.state.name}/>
+        <PureComp name={this.state.name}/> */}
       </div>
     )
   }
